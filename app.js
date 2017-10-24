e2871a728cfea57ce9a4f38e84af92b125729b97
Kev Comedia <kevcomedia@gmail.com>
2017-10-24 18:23:58 +0800

    Write route for file upload

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
+const upload = multer();
 
 app.get('/', function(req, res) {
   res.send('Hello world');
+});
+
+app.post('/upload', upload.single('file'), function(req, res) {
+  const {size} = req.file;
+  res.json({size});
 });
 
 module.exports = app;
